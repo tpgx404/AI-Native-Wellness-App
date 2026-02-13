d1e6f1e14768fb2ec395beca62582646f8f84018
tpgx404 <thanmayipgowda@gmail.com>
2026-02-13 11:50:55 +0530

    fix the lines of App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { DreamState, DreamRecord, InputMode, DreamType, Mood, Theme } from './types';
 import { analyzeDream } from './services/geminiService';
